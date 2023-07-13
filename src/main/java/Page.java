c6e3f65820bf6390adf745b68358f2f0160aad89
Olga Gasich <olagasich@gmail.com>
2023-07-13 19:48:05 +0300

    refactor getErrorMessage method

--- src/main/java/Page.java
+++ src/main/java/Page.java
@@ -1,6 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class Page {
 
@@ -47,7 +51,8 @@ public class Page {
     }
 
     public String getErrorMessage(){
-        WebElement errorMessage = driver.findElement(By.xpath(errorMessageLocator));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+        WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(errorMessageLocator)));
         String errorMessageText = errorMessage.getText();
         return errorMessageText;
     }
